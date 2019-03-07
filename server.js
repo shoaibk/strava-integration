fb8c4876271888e2070619639218692bbce489a3
Shoaib Khan <shb.khan@gmail.com>
2019-03-07 14:33:33 -0500

    Update callback url

--- server.js
+++ server.js
@@ -11,5 +11,8 @@ app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/commute-tracker/index.html'));
 });
 
+app.get('/code_callback', (req, res) => res.send('Hello World!'))
+
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
