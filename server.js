bebe53bd1ee46a0eabe41c410a3e727b064c53e5
Shoaib Khan <shb.khan@gmail.com>
2019-03-18 13:35:10 -0400

    update

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/commute-tracker'));
 
-app.get('/code_callback', (req, res) => res.send('Got the code'));
+app.get('/code_callback', 
+    (req, res) => res.send('Got the code')
+);
 
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/commute-tracker/index.html'));
